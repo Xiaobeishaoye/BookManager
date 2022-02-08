74c7a7ee66e988e733653fe314874426c13b8be6
eureka <eurekame2000@163.com>
2022-02-08 19:10:13 +0800

    logout

--- src/main/java/book/manager/config/SecurityConfiguration.java
+++ src/main/java/book/manager/config/SecurityConfiguration.java
@@ -3,11 +3,18 @@ package book.manager.config;
 import book.manager.service.UserAuthService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +22,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Resource
     UserAuthService service;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()   //首先需要配置哪些请求会被拦截，哪些请求必须具有什么角色才能访问
+                .antMatchers("/static/**").permitAll()    //静态资源，使用permitAll来运行任何人访问（注意一定要放在前面）
+                .antMatchers("/**").hasRole("user")    //所有请求必须登陆并且是user角色才可以访问（不包含上面的静态资源）
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/doLogin")
+                .defaultSuccessUrl("/index",true)
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .and()
+                .csrf().disable();
+        }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/book/manager/controller/PageController.java
+++ src/main/java/book/manager/controller/PageController.java
@@ -11,4 +11,9 @@ public class PageController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
